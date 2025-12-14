54971934b108614c2a90d6fa0b9fe0c8f2ed450e
dominik <dominik.barukcic@fer.hr>
2025-12-14 13:04:29 +0100

    #2 | removed basic auth and added hashed for passwords into bootstrap

--- src/main/java/com/fer/apuw/lab/tweetie/config/SecurityConfiguration.java
+++ src/main/java/com/fer/apuw/lab/tweetie/config/SecurityConfiguration.java
@@ -4,26 +4,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf
-                        .ignoringRequestMatchers("/", "/h2-console/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
-                )
-                .headers(headers -> headers
-                        .frameOptions(frame -> frame.disable())
-                )
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/h2-console/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .formLogin(Customizer.withDefaults())
-                .build();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .csrf(AbstractHttpConfigurer::disable);
+        return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        return http
+//                .csrf(csrf -> csrf
+//                        .ignoringRequestMatchers("/", "/h2-console/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+//                )
+//                .headers(headers -> headers
+//                        .frameOptions(frame -> frame.disable())
+//                )
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/", "/h2-console/**", "/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(Customizer.withDefaults())
+//                .build();
+//    }
 
 }
